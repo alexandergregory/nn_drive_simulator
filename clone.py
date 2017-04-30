76c616858c22e7fdf5b01c179642d22dadeb48e8
Alex G <alexandergregory@me.com>
2017-04-30 02:58:51 +0000

    updated preprocessing - data augmentation through flipping input images and steering angles

--- clone.py
+++ clone.py
@@ -24,8 +24,15 @@ for line in lines:
     measurement = float(line[3])
     measurements.append(measurement)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image,1))
+    augmented_measurements.append(measurement*-1.0)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
