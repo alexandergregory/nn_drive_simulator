cd5c09b6ee0f158520dd9485b63d2e1803308d26
Alex G <alexandergregory@me.com>
2017-04-30 04:08:03 +0000

    updated model - changed crop parameters

--- clone.py
+++ clone.py
@@ -64,7 +64,7 @@ from keras.layers import Convolution2D, Cropping2D, MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5 , input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Cropping2D(cropping=((50,20), (0,0))))
 model.add(Convolution2D(6,5,5, activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6,5,5,activation="relu"))
