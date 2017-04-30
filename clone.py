c5eb9f6d88b83af340d3ed118265846e79886ba9
Alex G <alexandergregory@me.com>
2017-04-30 01:56:47 +0000

    removing some gitignored files and updating model

--- clone.py
+++ clone.py
@@ -35,7 +35,7 @@ model.add(Flatten(input_shape=(160,320,3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
 
