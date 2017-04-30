b853334416333eceb516546dd2463c41c1f18ab2
Alex G <alexandergregory@me.com>
2017-04-30 02:18:31 +0000

    updated model - added lambda layer

--- clone.py
+++ clone.py
@@ -28,14 +28,15 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
 
 model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5 , input_shape=(160,320,3)))
 model.add(Flatten(input_shape=(160,320,3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.3, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
 
