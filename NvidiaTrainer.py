c6a6230cb74e6785f9e99db2b1a0630649f025f5
alexandergregory <greal620@student.otago.ac.nz>
2017-05-01 15:15:46 +1200

    updated nvidia architecture with generator:

--- NvidiaTrainer.py
+++ NvidiaTrainer.py
@@ -1,6 +1,8 @@
 import csv
 import cv2
 import numpy as np
+import os
+import sklearn
 
 lines = []
 with open('data/driving_log.csv') as csvfile:
@@ -8,55 +10,70 @@ with open('data/driving_log.csv') as csvfile:
     for line in reader:
         lines.append(line)
         #print(line)
-print(lines[0])	
+# print(lines[0])	
 del(lines[0])
-print(lines[0])
+# print(lines[0])
 
-images = []
-measurements = []
-for line in lines:
-    center_source_path = line[0]
-    left_source_path = line[1]
-    right_source_path = line[2]
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
-    steering_center = float(line[3])
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1:
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
 
-    correction = 0.2 #can tune this parameter
+            images = []
+            measurements = []
+            for line in lines:
+                center_source_path = line[0]
+                left_source_path = line[1]
+                right_source_path = line[2]
 
-    center_filename = center_source_path.split('/')[-1]
-    left_filename = left_source_path.split('/')[-1]
-    right_filename = right_source_path.split('/')[-1]
+                steering_center = float(line[3])
+                correction = 0.2 #can tune this parameter
+
+                center_filename = center_source_path.split('/')[-1]
+                left_filename = left_source_path.split('/')[-1]
+                right_filename = right_source_path.split('/')[-1]
+                
+                center_path = 'data/IMG/' + center_filename
+                left_path = 'data/IMG/' + left_filename
+                right_path = 'data/IMG/' + right_filename
+
+                img_center = cv2.imread(center_path)
+                img_left = cv2.imread(left_path)
+                img_right = cv2.imread(right_path)
+                centre_measurement = float(line[3])
+                left_measurement = centre_measurement + correction
+                right_measurement = centre_measurement - correction
+
+                images.append(img_center)
+                measurements.append(centre_measurement)
+                images.append(img_left)
+                measurements.append(left_measurement)
+                images.append(img_right)
+                measurements.append(right_measurement)
+    
+            augmented_images, augmented_measurements = [], []
+            for image, measurement in zip(images, measurements):
+                augmented_images.append(image)
+                augmented_measurements.append(measurement)
+                augmented_images.append(cv2.flip(image,1))
+                augmented_measurements.append(measurement*-1.0)
+
+            X_train = np.array(augmented_images)
+            y_train = np.array(augmented_measurements)
+
+            yield sklearn.utils.shuffle(X_train, y_train)
+    
+
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
 #    print('filename:' , filename)
-    center_path = 'data/IMG/' + center_filename
-    left_path = 'data/IMG/' + left_filename
-    right_path = 'data/IMG/' + right_filename
 
-    img_center = cv2.imread(center_path)
-    img_left = cv2.imread(left_path)
-    img_right = cv2.imread(right_path)
-
-
-    centre_measurement = float(line[3])
-    left_measurement = centre_measurement + correction
-    right_measurement = centre_measurement - correction
-
-    images.append(img_center)
-    measurements.append(centre_measurement)
-    images.append(img_left)
-    measurements.append(left_measurement)
-    images.append(img_right)
-    measurements.append(right_measurement)
-
-
-augmented_images, augmented_measurements = [], []
-for image, measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image,1))
-    augmented_measurements.append(measurement*-1.0)
-
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
@@ -77,7 +94,9 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.3, shuffle=True, nb_epoch=5)
+model.fit_generator(train_generator, samples_per_epoch= /
+            len(train_samples), validation_data=validation_generator, /
+            nb_val_samples=len(validation_samples), nb_epoch=3)
 
 model.save('Nvidiamodel.h5')
 
