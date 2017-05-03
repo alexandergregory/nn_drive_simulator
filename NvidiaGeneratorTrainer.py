5fd1c5dc7f7aa5f4ed0e0d8fd7acb299dc6c64e3
alexandergregory <alexandergregory@me.com>
2017-05-03 03:07:27 +0000

    ipdated augmented-generator model with dropout

--- NvidiaGeneratorTrainer.py
+++ NvidiaGeneratorTrainer.py
@@ -84,13 +84,15 @@ validation_generator = generator(validation_samples, batch_size=300)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
-from keras.layers import Convolution2D, Cropping2D, MaxPooling2D
+from keras.layers import Convolution2D, Cropping2D, MaxPooling2D, Dropout
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
 model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
+model.add(Dropout(0.2))
 model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
+model.add(Dropout(0.2))
 model.add(Convolution2D(48,5,5, subsample=(2,2), activation="relu"))
 model.add(Convolution2D(64,3,3, activation="relu"))
 model.add(Convolution2D(64,3,3, activation="relu"))
